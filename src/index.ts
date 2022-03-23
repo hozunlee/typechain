3a68187814ce9401ab9244da70590dfed09f48de
ho2yahh <ho2yahh@gmail.com>
2022-03-24 00:25:24 +0900

    class in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 export {};
 
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "hoya",
-    age: 24,
-    gender: "male",
-};
+const lynn = new Human("Lynn", 18, "female");
+
+// const person = {
+//     name: "hoya",
+//     age: 24,
+//     gender: "male",
+// } as Human;
 
 const sayHi = (person: Human): void => {
     console.log(
@@ -18,4 +25,4 @@ const sayHi = (person: Human): void => {
     );
 };
 
-sayHi(person);
+sayHi(lynn);
